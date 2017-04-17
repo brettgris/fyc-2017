1ea9859216a815f36441d840f0b2b9eb3236a1bf
brettgris <Brett.Grisinger@Starz.com>
2017-04-17 14:57:32 -0600

    trailer player updates

--- src/react/components/trailer/Trailer.jsx
+++ src/react/components/trailer/Trailer.jsx
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
-import Video from 'react-html5video';
 
 class Trailer extends Component{
 	render(){
-		if (this.props.episode.num!=="trailer") return null;
+		if (this.props.episode.num!=="trailer" || this.props.show.trailer==="") return null;
 		return(
 			<div className="trailer">
 				<div className="trailer-container">
 					<Link className="exitbtn" to={`/${this.props.guild}/${this.props.show.safename}`}>
 						<img src="/img/exit.png" />
 					</Link>
-					<Video width="100%" controls unmuted autoPlay>
-						<source src={`../img/video/${this.props.show.safename}.mp4`} type="video/mp4" />
-	  				</Video>
+					<div className="video-container">
+						<div className="video-player">
+							<iframe src={this.props.show.trailer+"?footer=false&cid=starzfyc2017trailer"} frameBorder="0" scrolling="no" width="100%" height="100%"></iframe>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
