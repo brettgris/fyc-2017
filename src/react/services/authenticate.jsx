1e064b14ece2c743a7c22819025ee04cd258ea90
brettgris <Brett.Grisinger@Starz.com>
2017-04-20 16:42:12 -0600

    Update

--- src/react/services/authenticate.jsx
+++ src/react/services/authenticate.jsx
@@ -10,15 +10,18 @@ class Authenticate{
 
 		//"37a7dac83cffb918fa54a6e9c2d4cfc0109dc79cc94adffdfc7a71794c2fd7f5"
 		this.values = {
-			emmys: "1B3BFEA7C77F1C4CA1464451438A7FE1072EF83840D0955B0CC9DDF8D5FA4AD7"
+			emmys: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 		}
 	}
 
 	check(obj){
 		obj.a = obj.a || {};
 
+		//console.log( $.cookie( this.ck[obj.guild] ), this.values[obj.guild] )
+
 		if (obj.num && obj.num!=="trailer") {
 			if ( $.cookie( this.ck[obj.guild] ) !== this.values[obj.guild]) {
+
 				if (CryptoJS.SHA256(obj.a[obj.guild])==this.values[obj.guild]){
 					obj.login = false;
 					obj.error = false;
